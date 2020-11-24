82b1a083cb86b35f2310fd94d15d4cfd0651c7fb
david cortesi <david.cortesi.097@gmail.com>
2020-11-24 17:02:41 +0100

    commit 2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const app = new Vue({
   el: ".app",
   data:{
-    cds:[]
+    cds:[],
+    actualGenre: "all"
   },
   created(){
     axios.get("https://flynn.boolean.careers/exercises/api/array/music")
@@ -16,4 +17,26 @@ const app = new Vue({
       this.errored = true
     } );
   },
+  methods:{
+    filterByGenre(actualGenre){
+      if ( actualGenre === "all" ) {
+        axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then( result => {
+          albumList = result.data.response;
+          this.cds = albumList;
+          console.log( this.cds );
+          return albumList;
+        })
+        .catch( error => {
+          console.log(error)
+          this.errored = true
+        } )
+      }
+      else {
+        this.cds = result.data.response.filter( (cd) => {
+          result.data.response === actualGenre;
+        })
+      }
+    }
+  }
 });
